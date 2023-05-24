87a7d4ce12f5cc8088aa3e6f0bc57847e92228cd
Albert Shubin <albert.shubin@gmail.com>
2023-05-25 00:25:07 +0400

    refactor: used interpolation instead of concatenation

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const hasUserName = () => {
+const hasUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + userName);
+  console.log(`Hello, ${userName}`);
 };
+
+export default hasUserName;
