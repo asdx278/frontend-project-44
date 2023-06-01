4ebd4ff5b734c42cafda96e17cf93337674694f1
Albert Shubin <albert.shubin@gmail.com>
2023-06-02 00:45:33 +0400

    refactor: typo fixed

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ export default (rules, gameProcess) => {
       console.log(
         `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`
       );
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       break;
     }
   }
